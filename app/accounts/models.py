66fc7ba701fc3ac3074784d7f8be2caefd12e15a
krytofin <kzn.dragons.craft@gmail.com>
2025-09-06 16:06:24 +0300

    feat: add needed fields (is_admin, is_verificated) in User model in accounts app

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -4,14 +4,17 @@ from django.db import models
 
 
 class User(AbstractBaseUser):
-    uuid = models.UUIDField(primary_key=True, default=uuid4)
-    username = models.CharField(max_length=70)
+    uuid = models.UUIDField(primary_key=True, default=uuid4, unique=True)
+    username = models.CharField(max_length=70, unique=True)
     email = models.EmailField()
     friends = models.ManyToManyField("self", blank=True, symmetrical=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     description = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
+    
+    is_admin = models.BooleanField(default=False)
+    is_verificated = models.UUIDField(default=uuid, unique=True)
     
     def __str__(self):
         return f'{self.username} {self.email} {self.first_name} {self.last_name}'
