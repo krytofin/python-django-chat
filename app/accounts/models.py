0a823e2fe6eb202a635a96deaac6a404a95dd560
krytofin <kzn.dragons.craft@gmail.com>
2025-09-06 16:03:33 +0300

    feat: add User model in accounts app

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -1,3 +1,17 @@
+from uuid import uuid4
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    uuid = models.UUIDField(primary_key=True, default=uuid4)
+    username = models.CharField(max_length=70)
+    email = models.EmailField()
+    friends = models.ManyToManyField("self", blank=True, symmetrical=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'{self.username} {self.email} {self.first_name} {self.last_name}'
