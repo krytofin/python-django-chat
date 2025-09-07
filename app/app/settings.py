887099c9d1211b0eae68acc820b888a3b65530d4
krytofin <kzn.dragons.craft@gmail.com>
2025-09-07 22:47:04 +0300

    feat: set default user model to my model

--- app/app/settings.py
+++ app/app/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# set User model to my model
+AUTH_USER_MODEL= 'accounts.User'
