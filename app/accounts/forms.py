95bd71cf641af2cf56c4ef30c4a6373cdf560668
krytofin <kzn.dragons.craft@gmail.com>
2025-09-08 23:26:12 +0300

    feat: add RegisterForm to forms file in accounts app

--- app/accounts/forms.py
+++ app/accounts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth import get_user_model
 
 
 class LoginForm(forms.Form):
@@ -7,3 +8,28 @@ class LoginForm(forms.Form):
     
     
     
+class RegisterForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(
+        attrs={'class': 'form-elem'}))
+    password2 = forms.CharField(label='Password confirmation',
+                                widget=forms.PasswordInput(attrs={'class': 'form-elem'}))
+
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'email', 'first_name', 'last_name', 'description')
+        
+        
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        else:
+            return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
