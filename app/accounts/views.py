1e36840502e3556b9d247ebc60d14d8877f435af
krytofin <kzn.dragons.craft@gmail.com>
2025-09-08 23:32:55 +0300

    feat: add login_view in account app

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import HttpRequest
+from django.shortcuts import redirect, render
+
+from .forms import LoginForm
+from .models import User
 
 
+def user_login_view(request:HttpRequest):
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = LoginForm(request.POST)
+            if form.is_valid():
+                cleaned_data = form.cleaned_data
+                if User.objects.filter(username=cleaned_data['username']).exists():
+                    user = authenticate(request, username=cleaned_data['username'])
+                    if user:
+                        login(request, user)
+        else:
+            form = LoginForm()
+        return render(request, 'accounts/login.html', {'form':form})
+    else:
+        return redirect()
