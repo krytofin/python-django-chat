a9502438fcd239637611fb2cb0f00fa4470bbbea
krytofin <kzn.dragons.craft@gmail.com>
2025-09-09 23:55:41 +0300

    feat: basic setup for chat app

--- app/chat/views.py
+++ app/chat/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
-# Create your views here.
+def index_view(request:HttpRequest):
+    return render(request, 'chat/index.html')
