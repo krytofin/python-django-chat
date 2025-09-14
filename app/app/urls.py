2985c18e31c57ccf9452a5dfef8ce80a5feb1f6c
krytofin <kzn.dragons.craft@gmail.com>
2025-09-14 14:37:21 +0300

    fix: accounts app url fix

--- app/app/urls.py
+++ app/app/urls.py
@@ -20,6 +20,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
     path('', include('chat.urls')),
 ]
