65c1499a27714c24e4bf7df7b97f8431968ff6e7
krytofin <kzn.dragons.craft@gmail.com>
2025-09-16 22:07:37 +0300

    feat: add models and migratings to chat app

--- app/chat/models.py
+++ app/chat/models.py
@@ -1,3 +1,21 @@
+from uuid import uuid4
+from django.contrib.auth import get_user_model
 from django.db import models
 
 # Create your models here.
+class Group(models.Model):
+    uuid = models.UUIDField(default=uuid4, unique=True)
+    users = models.ManyToManyField(get_user_model())
+    messages = models.ManyToManyField('Message')
+    events = models.ManyToManyField('Event')
+    time_created = models.DateTimeField(auto_now_add=True)
+    
+    
+    
+class Message(models.Model):
+    text = models.TextField()
+    sent_time = models.DateTimeField(auto_now_add=True)
+    
+class Event(models.Model):
+    type = models.TextField()
+    time = models.DateTimeField(auto_now_add=True)
